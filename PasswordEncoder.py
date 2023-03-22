5f905b371a1c4445262ae228b40f51638b111017
jayce <jaycecorcoran@gmail.com>
2023-03-22 18:44:36 -0400

    First Upload

---
+++ PasswordEncoder.py
@@ -0,0 +1,42 @@
+def encode(password):  #Creates encode for password by adding 3 to user input
+    newPass = ''
+    for x in password:
+        num = int(x)
+        if num == 9:
+            num = 2
+        elif num == 8:
+            num = 1
+        elif num == 7:
+            num = 0
+        else:
+            num = num + 3
+        newPass += str(num)
+    return newPass
+
+
+def menu():  #creates print menu
+    print("""
+    Menu
+    -----------
+    1. Encode
+    2. Decode
+    3. Quit
+    """)
+
+
+def main():  #run program
+    option = 0
+    userPass = ''
+    while option != 3:
+        main()
+        option = int(input("Please enter an option: "))
+        if option == 1:
+            userPass = input("Please enter a password to encode: ")
+            print("Your password has been encoded and stored!")
+            userPass = encode(userPass)
+        if option == 2:
+            print(f"The encoded password is {userPass} and the original password is ")
+
+
+if __name__ == '__main__':  #make code work
+    main()
